c86a7d7948775b8f2670df657045567ab8a90b1e
Natalie Morales <nataliemorales996@gmail.com>
2024-04-25 01:11:38 +0000

    messaging

--- Phase-2-Backend-Java-and-Database-Development/02-Day-4-JDBC-Sample-Program/src/main/java/com/frank/exceptions/DataBaseDeleteException.java
+++ Phase-2-Backend-Java-and-Database-Development/02-Day-4-JDBC-Sample-Program/src/main/java/com/frank/exceptions/DataBaseDeleteException.java
@@ -1,5 +1,15 @@
 package com.frank.exceptions;
 
+// This is a custom exception used when there is a data base delete error 
+// We wanted to provide more granularity to a problem than the generic DataAccessException provided by SpringJDBC
+
+// To define a custom exception:
+//
+//		1. Make it a subclass of exception
+//		2. Provide a default constructor that calls the Exception constructor (super())
+//		3. Provide a constructor that takes a string parameter (error message) and pass it to super constructor
+//		4. Anything else you think you want in the custom exception
+
 public class DataBaseDeleteException extends Exception {
 
 	public DataBaseDeleteException() {
